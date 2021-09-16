d5e382aa1eed0bab9528794eca16b92f3ee6a56b
Hamsa <hamsam0097@gmail.com>
2021-09-16 17:03:58 +0100

    testing and develop Gameboard

--- src/gameboard.js
+++ src/gameboard.js
@@ -3,13 +3,20 @@ import ShipFactory from './ship.js'
 const Gameboard = () => ({
     place : (coordinate, ship) => {
         if (ship.orientation === 'horizontal'){
-            let array = [];
-            let finalCoordinate = coordinate + ship.length
-            for (let i=coordinate; i<finalCoordinate; i++){
-                array.push(i);
-            }
-            return array.values();
+            let finalCoordinate = coordinate + ship.length;
+            returnPlacedCoordinates(coordinate, finalCoordinate)
         }
+        return returnedCoordinates;
+    },
+    returnPlacedCoordinates : (coordinate, finalCoordinate) => {
+        let returnedCoordinates = [];
+        for (let i=coordinate; i<=finalCoordinate; i++){
+            returnedCoordinates.push(i);
+        }
+        return returnedCoordinates
     }
 });
 
+Gameboard().returnPlacedCoordinates(11,14);
+
+export default Gameboard
--- src/test/gameboard.test.js
+++ src/test/gameboard.test.js
@@ -2,7 +2,14 @@ import Gameboard from '../gameboard.js';
 import ShipFactory from '../ship.js';
 
 
-test('Ship is placed horizontally', () => {
+test('Gameboard place function is producing coordinates', () => {
     const ship = ShipFactory(2, 'horizontal');
-    expect((Gameboard().place(11, ship))).toBe(11, 12)
+    expect((Gameboard().place(11, ship))).toBeDefined()
+  });
+
+  test('Gameboard producing correct place coordinates', () => {
+    expect(
+      (Gameboard().place(11, 12)).includes(11) &&
+      (Gameboard().place(11, 13)).includes(12)
+      .toBe(true))
   });
